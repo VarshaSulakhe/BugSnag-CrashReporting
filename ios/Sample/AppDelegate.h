f07b57839afdd2d62ea7f9645fcec512be9e196f
vh <varsha.sulakhe@gmail.com>
2017-06-15 11:26:11 +0530

    Sample code works

---
+++ ios/Sample/AppDelegate.h
@@ -0,0 +1,16 @@
+/**
+ * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+#import <UIKit/UIKit.h>
+#import <Bugsnag/Bugsnag.h>
+#define BUGSNAG_APP_ID @"df4f44f70a3899f597344f8ffa72026f"
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (nonatomic, strong) UIWindow *window;
+@end
